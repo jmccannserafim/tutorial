c00fa3b0011df47f1c6b4d67760d26d46b99265d
jmccannserafim <jim.mccann@serafimltd.com>
2017-04-10 12:13:02 +0100

    second summit

--- tutorial1/main.cpp
+++ tutorial1/main.cpp
@@ -7,9 +7,27 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <string>
 
 int main(int argc, const char * argv[]) {
     // tutorial 1
-    std::cout << "Hello, World!\n";
+    
+    const double pi=M_PI;
+    char vol;
+    int i=5, j=2;
+    int array1[2];
+    
+    double x=1.e0, y=2.e0, z=3.e0 ,half=0.5 ;
+    double w;
+    
+    vol='t';
+    array1[0]={4};
+    array1[1]=2.;
+
+    w=pow(y,half);
+    w=i/(double(j));
+    std::cout << "\n" << w << "\n";
+    std::cout << "\n" << vol << "\n" << array1[1] << "\n";
     return 0;
 }
