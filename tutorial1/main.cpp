0c0bf8b7bfafb07fcdfcdffea0020fb1a32eed94
jmccannserafim <jim.mccann@serafimltd.com>
2017-04-10 11:45:14 +0100

    Initial Commit

---
+++ tutorial1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tutorial1
+//
+//  Created by Jim McCann on 10/04/2017.
+//  Copyright © 2017 Jim McCann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
